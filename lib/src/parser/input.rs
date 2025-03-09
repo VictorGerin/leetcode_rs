9a3e6f855c56ee38869b5895a0490d404bc949b3
VictorGerin <gerinlacerda@gmail.com>
2025-03-09 15:02:16 -0300

    remove read from stdin, now using only file input for for better debug and testing

--- lib/src/parser/input.rs
+++ lib/src/parser/input.rs
@@ -1,24 +1,13 @@
-use std::io::{self, BufReader, Read, IsTerminal};
+use std::io::{BufReader, Read};
 
 pub fn read_input(default_file: &str) -> Result<std::vec::IntoIter<char>, String> {
-    let chars: std::vec::IntoIter<char> = if io::stdin().is_terminal() {
-        let file = std::fs::File::open(default_file)
-            .map_err(|err| format!("Error reading input: {}", err))?;
-        
-        let bytes: Vec<u8> = BufReader::new(file)
-            .bytes()
-            .filter_map(|b| b.ok())
-            .collect();
-                    
-        String::from_utf8_lossy(&bytes).chars().collect::<Vec<char>>().into_iter()
-    } else {
-        let bytes: Vec<u8> = io::stdin()
-            .bytes()
-            .filter_map(|b| b.ok())
-            .collect();
-            
-        String::from_utf8_lossy(&bytes).chars().collect::<Vec<char>>().into_iter()
-    };
-
-    Ok(chars)
+    let file = std::fs::File::open(default_file)
+        .map_err(|err| format!("Error reading input: {}", err))?;
+    
+    let bytes: Vec<u8> = BufReader::new(file)
+        .bytes()
+        .filter_map(|b| b.ok())
+        .collect();
+                
+    Ok(String::from_utf8_lossy(&bytes).chars().collect::<Vec<char>>().into_iter())
 } 
