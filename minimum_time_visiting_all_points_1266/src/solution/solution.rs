95a5dec77529f6ba551265a18e59bdb4bd5781a5
luffy <gerinlacerda@gmail.com>
2026-01-12 00:20:52 -0300

    Add minimum_time_visiting_all_points_1266 package and update workspace configuration in Cargo.toml and Cargo.lock

---
+++ minimum_time_visiting_all_points_1266/src/solution/solution.rs
@@ -0,0 +1,11 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+        points.windows(2).map(|pair| {
+            let [x1, y1] = [pair[0][0], pair[0][1]];
+            let [x2, y2] = [pair[1][0], pair[1][1]];
+            (x2 - x1).abs().max((y2 - y1).abs())
+        }).sum()
+    }
+}
---
+++ minimum_time_visiting_all_points_1266/src/solution/test.rs
@@ -0,0 +1,35 @@
+#[cfg(test)]
+
+use super::solution::*;
+
+#[test]
+fn minimum_time_visiting_all_points_1266() -> Result<(), String> {
+    use leetcode_lib::parser::{Val, ValIter, ProcessInputError, read_input};
+    
+    ValIter::new(read_input("input.txt")?)
+        .collect::<Result<Vec<Val>, ProcessInputError>>()
+        .map_err(|e| e.to_string())?
+        .chunks_exact(2)
+        .try_for_each(|caso_teste| -> Result<(), String> {
+            let points = caso_teste[0].clone()
+                .as_vec()
+                .map_err(|x| format!("Expected array, got {:?}", x))?
+                .into_iter()
+                .map(|x| -> Result<Vec<i32>, String> {
+                    Ok(x.as_vec_int()
+                    .map_err(|x| format!("as_vec_int: {:?}", x))?)
+                }).collect::<Result<Vec<Vec<i32>>, String>>()?;
+
+            let expected = caso_teste[1].clone()
+                .as_int()
+                .map_err(|x| format!("Expected integer, got {:?}", x))?;
+
+            let result = Solution::min_time_to_visit_all_points(points.clone());
+            assert_eq!(result, expected, "Expected result to be {}, but got {} for input {:?}", expected, result, points);
+
+            Ok(())
+        })?;
+
+    println!("All test cases passed!");
+    Ok(())
+}
